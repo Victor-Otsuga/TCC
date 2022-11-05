16785f1c4be0b51415eb6e5ed243174d4f27fce7
Victor Augusto <105857027+Victor-Otsuga@users.noreply.github.com>
2022-11-04 21:16:12 -0300

    remoção da versão antiga

--- TCC2-master/PHP/listaclifunc.php
+++
@@ -1,9 +0,0 @@
-<?php
-//seleciona no banco os dados necessarios
-$selectconta="SELECT * FROM cliente ORDER BY id_cli asc";
-
-
-//pega os resultados selecionados
-$result = $pdo->prepare($selectconta);
-
-$result->execute();
