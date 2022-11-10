77edd5174d74912057e26e109d2e0de671cd8829
Victor Augusto <105857027+Victor-Otsuga@users.noreply.github.com>
2022-11-10 20:42:21 -0300

    Add files via upload

---
+++ TCC-main/JAVASCRIPT/carrinho.js
@@ -0,0 +1,2 @@
+document.getElementById ("carBtn").addEventListener("click", function(){ alert("Hello World!")})
+
---
+++ TCC-main/PHP/pedido.php
@@ -0,0 +1,238 @@
+<?php
+include ('verificarLogin.php');
+include ('conexao.php'); 
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../CSS/Menu.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/pedido.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/bootstrap.min.css">
+    <title>Pedido</title>
+</head>
+
+<body>
+
+   
+    <!--Conteúdo do Site-->
+  
+        <div class="pedidoex">
+          <div class="headerp">
+            <a href="Menu.php"id="seta" > 
+            <img src="../IMG/imgseta.png" alt="">
+            </a>
+            <div class="headerpedido">   
+            
+                <p>Cliente:&nbsp;&nbsp;
+                    &nbsp;Variavel nome - variavel id</p>
+            </div>
+              <div class="headerpedido">   
+                
+                <p>Data:&nbsp;&nbsp;
+                    &nbsp; xx/xx/xx</p>
+            </div>
+           </div>
+           <div id="conteudo">
+            
+            
+            <div class="tabelafinal" href="pedido.php">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Produto</th>
+                            <th scope="col">Tipo</th>
+                            <th scope="col">Quantidade</th>
+                            <th scope="col">Preço Unidade</th>
+                            <th scope="col">Preço Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Morango</td>
+                            <td>Massa 5L</td>
+                            <td>10</td>
+                            <td>R$10,00</td>
+                            <td> R$100,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Chocolate</td>
+                            <td>Massa 1L</td>
+                            <td>10</td>
+                            <td>R$9,00</td>
+                            <td> R$90,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Limão</td>
+                            <td>Massa 5L</td>
+                            <td>10</td>
+                            <td>R$10,00</td>
+                            <td> R$100,00</td>
+                        </tr>
+
+
+
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Morango</td>
+                            <td>Massa 5L</td>
+                            <td>10</td>
+                            <td>R$10,00</td>
+                            <td> R$100,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Chocolate</td>
+                            <td>Massa 1L</td>
+                            <td>10</td>
+                            <td>R$9,00</td>
+                            <td> R$90,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Morango</td>
+                            <td>Massa 5L</td>
+                            <td>10</td>
+                            <td>R$10,00</td>
+                            <td> R$100,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Chocolate</td>
+                            <td>Massa 1L</td>
+                            <td>10</td>
+                            <td>R$9,00</td>
+                            <td> R$90,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Morango</td>
+                            <td>Massa 5L</td>
+                            <td>10</td>
+                            <td>R$10,00</td>
+                            <td> R$100,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Chocolate</td>
+                            <td>Massa 1L</td>
+                            <td>10</td>
+                            <td>R$9,00</td>
+                            <td> R$90,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Morango</td>
+                            <td>Massa 5L</td>
+                            <td>10</td>
+                            <td>R$10,00</td>
+                            <td> R$100,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Chocolate</td>
+                            <td>Massa 1L</td>
+                            <td>10</td>
+                            <td>R$9,00</td>
+                            <td> R$90,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Morango</td>
+                            <td>Massa 5L</td>
+                            <td>10</td>
+                            <td>R$10,00</td>
+                            <td> R$100,00</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Chocolate</td>
+                            <td>Massa 1L</td>
+                            <td>10</td>
+                            <td>R$9,00</td>
+                            <td> R$90,00</td>
+                        </tr>
+
+                        
+
+
+                    </tbody>
+                </table>
+            </div>
+
+
+        </div>
+
+
+</div>
+
+    </div>
+
+    
+
+    <!--Menu Vertical-->
+
+    <div id="menuho">
+    <div id="operador">
+        <!-- <img  id="icon" alt=""> -->
+
+        <?php 
+        
+        $avatar = $_SESSION['avatar_session']; 
+
+        echo '<img id="icon" src="'.$avatar.'">';
+        
+        ?>
+            
+            <h1 id="nome">Operador: <?php
+               
+                $nome_oper = $_SESSION['nome_session'];           
+                echo $nome_oper ?></h1>
+
+        </div>
+
+
+
+
+        
+        <a class="sidebtn" href="MontarPedido.php"> <img class="imgbtn" src="../IMG/MP.png">
+            <div class="MP"> Montar Pedido</div>
+        </a>
+        <a class="sidebtn" href="historico.php"> <img class="imgbtn" src="../IMG/historico.png">
+            <div href="#" class="MP">Histórico de Vendas</div>
+        </a>
+        <a class="sidebtn" href="ProdutosList.php"> <img class="imgbtn" src="../IMG/LP.png">
+            <div href="#" class="MP">Lista de Produtos</div>
+        </a>
+        </a>
+        <a class="sidebtn" href="listagemcli.php"> <img class="imgbtn" src="../IMG/LC.png">
+            <div href="#" class="MP">Lista de
+                Clientes</div>
+        </a>
+        <a class="sidebtn" href="logout.php"> <img class="imgbtn" src="../IMG/EX.png">
+            <div href="#" class="MP">Sair</div><p id="sairadjustment"></p>
+        </a>
+        <img src="../IMG/trevoice.png" class="logo" alt="">
+
+    </div>
+</body>
