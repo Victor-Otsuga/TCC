d1c1b4d0a17bdcb687e0d217d901268faeb0e979
Victor Augusto <105857027+Victor-Otsuga@users.noreply.github.com>
2022-11-17 22:12:36 -0300

    PHP com links organizados

---
+++ PHP/Produtos/listaprodbusca.php
@@ -0,0 +1,55 @@
+<?php
+
+include('../conexao.php');
+
+
+$pesquisa =  $_POST['pesprod'];
+
+$query = $pdo->prepare("SELECT * FROM produtos WHERE sabor LIKE '%$pesquisa%' ORDER BY sabor");
+$query->execute(array($pesquisa));
+
+$num = $query->rowCount();
+if ($num > 0) {
+
+?>
+
+
+    <!-- <<div   style="margin-left: 10%; "  href="pedido.php">
+                <table  class="table" id="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Sabor</th>
+                        <th scope="col">Tipo</th>
+                        <th scope="col">Tamanho</th>
+                        <th scope="col">Preços</th>
+                        <th scope="col">Editar</th>
+                        
+                      </tr>
+                    </thead>
+                    <tbody> -->
+
+                    <?php
+                        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {  ?>
+                      <tr>
+                        <td><?php echo $row["id_prod"]; ?></td>
+                        <td><?php echo $row["sabor"]; ?></td>
+                        <td><?php echo $row["tipo"]; ?></td>
+                        <td><?php echo $row["tamanho_uni"]; ?></td>
+                        <td><?php echo $row["preco_uni"]; ?></td>
+                        <td ><Button class="btn">Editar</Button></td>
+                      </tr>
+                      <?php  }; ?>
+                    </tbody>
+                    
+                  </table>
+                </div
+                >
+                <?php
+    
+
+    
+} else {
+    echo "Esta Pessoa Não Existe!";
+}
+?>
---
+++ PHP/Produtos/search.php
@@ -0,0 +1,58 @@
+<?php
+
+include('../conexao.php');
+
+//listagem de produtos e consulta
+
+if (isset($_POST["nome"])) {
+	$busca = $_POST["nome"];
+	$query = "select * from produtos where sabor like '%".$busca."%' order by sabor";
+}
+else {
+	$query = "select * from produtos order by sabor";
+}
+$statement = $pdo->prepare($query);
+$statement->execute();
+$result = $statement->fetchAll();
+$rowCount = $statement->rowCount();
+
+if ($rowCount > 0) {
+	$data = '<div   style="margin-left: 10%; "  href="../Venda/pedido.php">
+	<table  class="table" id="table">
+	<thead>
+		<tr>
+			<th scope="col">ID</th>
+			<th scope="col">Sabor</th>
+			<th scope="col">Tipo</th>
+			<th scope="col">Tamanho</th>
+			<th scope="col">Preço da unidade</th>
+			<th scope="col">Preço do pacote</th>
+			<th scope="col">Tamanho do pacote</th>
+			<th scope="col">Editar</th>
+		</tr>
+		</thead>';
+
+	foreach($result as $row) {
+		$data .= '<tbody>
+			<tr>
+				<td>'.$row["id_prod"].'</td>
+				<td>'.$row["sabor"].'</td>
+				<td>'.$row["tipo"].'</td>
+				<td>'.$row["tamanho_uni"].'</td>
+				<td>'.$row["preco_uni"].'</td>
+				<td>'.$row["preco_pacote"].'</td>
+				<td>'.$row["tamanho_pacote"].'</td>
+				<td><a href="editarprod.php?id='.$row["id_prod"].'"><Button class="btn">Editar</Button></a></td>
+				
+			</tr>
+		';
+	}
+	$data .= '</tbody>
+	</table>
+  </div>';
+}
+else {
+	$data = "Nenhum registro localizado.";
+}
+
+echo $data;
