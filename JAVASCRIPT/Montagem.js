b9b236cd89ac2b1102a4f725ce5e0180899ddb3f
Victor Augusto <105857027+Victor-Otsuga@users.noreply.github.com>
2022-11-30 20:39:00 -0300

    carrinho 95 por cento feito

--- JAVASCRIPT/Montagem.js
+++ JAVASCRIPT/Montagem.js
@@ -1,10 +1,70 @@
 var massas = localStorage.getItem('massav');
 var picole = localStorage.getItem('picolev');
 var coberturas = localStorage.getItem('coberturav');
-
-
+var verify = 0
+var identificador = 0
+var valor = 0
+var total = 0
+var add = 0
+var preco = 0
 
 function abrirAba(){
+
+
+while (verify < 200){
+if (verify ==1){
+        valor = 0
+        total = 0
+        add = 0
+        preco = 0
+        
+}
+     identificador = document.getElementById("qtn".concat(verify))
+
+     if (identificador != null){
+        valor = document.getElementById("qtn".concat(verify)).value
+        preco = document.getElementById("ppct".concat(verify)).value
+
+        document.getElementById("qtn".concat(verify)).value = localStorage.getItem('qtn'.concat(verify));
+
+        if (document.getElementById("qtn".concat(verify)).value == ""){
+                document.getElementById("qtn".concat(verify)).value = 1
+                localStorage.setItem("qtn", verify);
+        }
+        
+        if (valor == null || "" || NaN){
+        add = 0
+        }else{
+        valor = localStorage.getItem("qtn".concat(verify));
+        
+        add = (valor * preco)
+       
+
+        
+        total = total + add
+        
+        }
+
+
+     }else{
+        add= 0
+     }
+
+        verify+=1    
+       
+if (verify ==200){
+        
+        total = parseFloat(total).toFixed(2)
+        localStorage.setItem("totalv", total);
+        
+}
+     
+}
+
+
+
+
+
 
 
 
@@ -48,9 +108,6 @@ document.getElementById("tableC").style.display = "none";
         
 }
 }
-
-
-
 
 
 
@@ -161,3 +218,5 @@ function removeqtd(){
 
 
       
+
+       
