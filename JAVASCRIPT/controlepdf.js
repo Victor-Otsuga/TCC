b6bd3c896a6189435409cea8cabd163551b69b94
Victor Augusto <105857027+Victor-Otsuga@users.noreply.github.com>
2022-12-03 01:15:51 -0300

    Add files via upload

--- JAVASCRIPT/controlepdf.js
+++ JAVASCRIPT/controlepdf.js
@@ -1,11 +1,11 @@
  function download(){
     const item = document.querySelector(".tabelafinal");
     var opt = {
-        margin:       0.2,
-        filename:     'myfile.pdf',
+        margin:       0.3,
+        filename:     'venda.pdf',
         image:        { type: 'jpeg', quality: 0.98 },
         html2canvas:  { scale: 2 },
-        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait', size: 0.4 }
+        jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait', size: 0.4 }
         
       };
       // New Promise-based usage:
